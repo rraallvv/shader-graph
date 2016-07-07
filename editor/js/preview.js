4c3bc26dfe7b17db4c027122408d51106aab59ae
Developer <>
2016-07-07 17:39:58 -0400

    remove unused button images

--- editor/js/preview.js
+++ editor/js/preview.js
@@ -28,33 +28,6 @@ Editor.polymerElement({
 				]
 			},
 			"internal": {
-				"f0048c10-f03e-4c97-b9d3-3506e1d58952": [
-					"image/default_btn_normal",
-					"cc.SpriteFrame",
-					1
-				],
-				"e9ec654c-97a2-4787-9325-e6a10375219a": [
-					"image/default_btn_pressed",
-					"cc.SpriteFrame",
-					1
-				],
-				"29158224-f8dd-4661-a796-1ffab537140e": [
-					"image/default_btn_disabled",
-					"cc.SpriteFrame",
-					1
-				],
-				"e851e89b-faa2-4484-bea6-5c01dd9f06e2": [
-					"image/default_btn_normal.png",
-					"cc.Texture2D"
-				],
-				"b43ff3c2-02bb-4874-81f7-f2dea6970f18": [
-					"image/default_btn_pressed.png",
-					"cc.Texture2D"
-				],
-				"71561142-4c83-4933-afca-cb7a17f67053": [
-					"image/default_btn_disabled.png",
-					"cc.Texture2D"
-				]
 			}
 		},
 		"launchScene": "db://assets/Scene/Effect00.fire",
