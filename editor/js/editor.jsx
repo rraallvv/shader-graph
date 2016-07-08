d62cd3e9963b7c3775a67c6e32e35620b94241a7
Developer <>
2016-07-08 00:59:29 -0400

    move the cocos preview component to the graph

--- editor/js/editor.jsx
+++ editor/js/editor.jsx
@@ -347,6 +347,53 @@ var AddNodeButton = React.createClass({
 });
 
 var Preview = React.createClass({
+	_CCSettings: {
+		"platform": "web-desktop",
+		"groupList": [
+			"default"
+		],
+		"collisionMatrix": [
+			[
+				true
+			]
+		],
+		"rawAssets": {
+			"assets": {
+				"cd0305b4-c877-43af-8ed8-27d38478c262": [
+					"Images/monkey",
+					"cc.SpriteFrame",
+					1
+				],
+				"1d955528-ab82-4be4-a727-d864908c2b74": [
+					"Images/monkey.png",
+					"cc.Texture2D"
+				],
+				"ecbfd2e4-3595-4b9e-94e1-ad307f36bfab": [
+					"resources/EffectPreview.fs.glsl",
+					"cc.RawAsset"
+				],
+				"2fdfd899-b375-46c6-ac69-fdefaf994017": [
+					"resources/EffectPreview_noMVP.vs.glsl",
+					"cc.RawAsset"
+				],
+				"0bf9ac5e-ff33-4a0a-9de7-83bff3b29a40": [
+					"resources/EffectPreview.vs.glsl",
+					"cc.RawAsset"
+				]
+			},
+			"internal": {
+			}
+		},
+		"launchScene": "db://assets/Scene/EffectPreview.fire",
+		"scenes": [
+			{
+				"url": "db://assets/Scene/EffectPreview.fire",
+				"uuid": "0c0e54e8-a57a-4811-af7d-e0106440f9f2"
+			}
+		],
+		"orientation": "",
+		"debug": false
+	},
 	componentDidMount: function(){
 		var gooRunner = new goo.GooRunner({
 			logo: false,
@@ -374,12 +421,151 @@ var Preview = React.createClass({
 		}.bind(this), function () {
 			console.error('Error loading image.');
 		});
+
+		var self = this;
+
+		function boot () {
+
+			if ( !self._CCSettings.debug ) {
+				// retrieve minified raw assets
+				var rawAssets = self._CCSettings.rawAssets;
+				var assetTypes = self._CCSettings.assetTypes;
+				for (var mount in rawAssets) {
+					var entries = rawAssets[mount];
+					for (var uuid in entries) {
+						var entry = entries[uuid];
+						var type = entry[1];
+						if (typeof type === 'number') {
+							entry[1] = assetTypes[type];
+						}
+					}
+				}
+			}
+
+			// init engine
+			var canvas, div;
+			//var width = 640, height = 480;
+
+			if (cc.sys.isBrowser) {
+				canvas = document.getElementById('GameCanvas');
+				div = document.getElementById('GameDiv');
+
+				//width = div.clientWidth;
+				//height = div.clientHeight;
+			}
+
+			function setLoadingDisplay () {
+				// Loading splash scene
+				var splash = document.getElementById('splash');
+				var progressBar = splash.querySelector('.progress-bar span');
+				var currentResCount = cc.loader.getResCount();
+				cc.loader.onProgress = function (completedCount, totalCount, item) {
+					var percent = 100 * (completedCount - currentResCount) / (totalCount - currentResCount);
+					if (progressBar) {
+						progressBar.style.width = percent.toFixed(2) + '%';
+					}
+				};
+				splash.style.display = 'block';
+
+				cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
+					splash.style.display = 'none';
+				});
+			}
+
+			var onStart = function () {
+				cc.view.resizeWithBrowserSize(true);
+				// UC browser on many android devices have performance issue with retina display
+				if (cc.sys.os !== cc.sys.OS_ANDROID || cc.sys.browserType !== cc.sys.BROWSER_TYPE_UC) {
+					cc.view.enableRetina(true);
+				}
+				//cc.view.setDesignResolutionSize(self._CCSettings.designWidth, self._CCSettings.designHeight, cc.ResolutionPolicy.SHOW_ALL);
+			
+				if (cc.sys.isBrowser) {
+					setLoadingDisplay();
+				}
+
+				if (self._CCSettings.orientation === 'landscape') {
+					cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
+				}
+				else if (self._CCSettings.orientation === 'portrait') {
+					cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
+				}
+
+				// init assets
+				cc.AssetLibrary.init({
+					libraryPath: shaderGraph.resolveUrl('res/import'),
+					rawAssetsBase: shaderGraph.resolveUrl('res/raw-'),
+					rawAssets: self._CCSettings.rawAssets
+				});
+
+				var launchScene = self._CCSettings.launchScene;
+
+				// load scene
+				cc.director.loadScene(launchScene, null,
+					function () {
+						if (cc.sys.isBrowser) {
+							// show canvas
+							canvas.style.visibility = '';
+							var div = document.getElementById('GameDiv');
+							if (div) {
+								div.style.backgroundImage = '';
+							}
+						}
+
+						// play game
+						// cc.game.resume();
+
+						console.log('Success to load scene: ' + launchScene);
+					}
+				);
+
+				// purge
+				//noinspection JSUndeclaredVariable
+				self._CCSettings = undefined;
+			};
+
+			var option = {
+				//width: width,
+				//height: height,
+				id: 'GameCanvas',
+				scenes: self._CCSettings.scenes,
+				debugMode: self._CCSettings.debug ? cc.DebugMode.INFO : cc.DebugMode.ERROR,
+				showFPS: self._CCSettings.debug,
+				frameRate: 60,
+				jsList: [
+					shaderGraph.resolveUrl('js/project.js')
+				],
+				groupList: self._CCSettings.groupList,
+				collisionMatrix: self._CCSettings.collisionMatrix
+			};
+
+			cc.game.run(option, onStart);
+		}
+
+		if (cc.sys.isBrowser) {
+			boot();
+		}
+		else if (cc.sys.isNative) {
+			require('js/settings.js');
+			require('js/jsb_polyfill.js');
+
+			boot();
+		}
 	},
 	render: function() {
 		this.updateShader();
-		return React.createElement("div", null,
-			React.createElement("div", {className: "preview col-xs-12 style-scope shader-graph"}),
-			React.createElement("preview-view", {id: "preview"})
+		return (
+			<div>
+				<div className="preview col-xs-12 style-scope shader-graph"></div>
+				<div id="GameDiv" style={{width: "226px", height: "226px"}} className="style-scope shader-graph">
+					<canvas id="GameCanvas" width={"226"} height={"226"} className="style-scope shader-graph"></canvas>
+					<div id="splash" className="style-scope shader-graph">
+						<div className="progress-bar stripes style-scope shader-graph">
+							<span style={{width: "0%"}} className="style-scope shader-graph"></span>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	},
 	updateShader: function(){
