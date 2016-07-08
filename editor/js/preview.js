93f03cbfc391c7c72a4295fc123ca2147e2e1105
Developer <>
2016-07-08 00:05:52 -0400

    remove the timer to init cocos

--- editor/js/preview.js
+++ editor/js/preview.js
@@ -52,136 +52,133 @@ Editor.polymerElement({
 	},
 	ready: function(){
 		var self = this;
-		setTimeout(function(){
 
-			function boot () {
+		function boot () {
 
-				if ( !self._CCSettings.debug ) {
-					// retrieve minified raw assets
-					var rawAssets = self._CCSettings.rawAssets;
-					var assetTypes = self._CCSettings.assetTypes;
-					for (var mount in rawAssets) {
-						var entries = rawAssets[mount];
-						for (var uuid in entries) {
-							var entry = entries[uuid];
-							var type = entry[1];
-							if (typeof type === 'number') {
-								entry[1] = assetTypes[type];
-							}
+			if ( !self._CCSettings.debug ) {
+				// retrieve minified raw assets
+				var rawAssets = self._CCSettings.rawAssets;
+				var assetTypes = self._CCSettings.assetTypes;
+				for (var mount in rawAssets) {
+					var entries = rawAssets[mount];
+					for (var uuid in entries) {
+						var entry = entries[uuid];
+						var type = entry[1];
+						if (typeof type === 'number') {
+							entry[1] = assetTypes[type];
 						}
 					}
 				}
-
-				// init engine
-				var canvas, div;
-				//var width = 640, height = 480;
-
-				if (cc.sys.isBrowser) {
-					canvas = document.getElementById('GameCanvas');
-					div = document.getElementById('GameDiv');
-
-					//width = div.clientWidth;
-					//height = div.clientHeight;
-				}
-
-				function setLoadingDisplay () {
-					// Loading splash scene
-					var splash = document.getElementById('splash');
-					var progressBar = splash.querySelector('.progress-bar span');
-					var currentResCount = cc.loader.getResCount();
-					cc.loader.onProgress = function (completedCount, totalCount, item) {
-						var percent = 100 * (completedCount - currentResCount) / (totalCount - currentResCount);
-						if (progressBar) {
-							progressBar.style.width = percent.toFixed(2) + '%';
-						}
-					};
-					splash.style.display = 'block';
-
-					cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
-						splash.style.display = 'none';
-					});
-				}
-
-				var onStart = function () {
-					cc.view.resizeWithBrowserSize(true);
-					// UC browser on many android devices have performance issue with retina display
-					if (cc.sys.os !== cc.sys.OS_ANDROID || cc.sys.browserType !== cc.sys.BROWSER_TYPE_UC) {
-						cc.view.enableRetina(true);
-					}
-					//cc.view.setDesignResolutionSize(self._CCSettings.designWidth, self._CCSettings.designHeight, cc.ResolutionPolicy.SHOW_ALL);
-				
-					if (cc.sys.isBrowser) {
-						setLoadingDisplay();
-					}
-
-					if (self._CCSettings.orientation === 'landscape') {
-						cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
-					}
-					else if (self._CCSettings.orientation === 'portrait') {
-						cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
-					}
-
-					// init assets
-					cc.AssetLibrary.init({
-						libraryPath: self.resolveUrl('res/import'),
-						rawAssetsBase: self.resolveUrl('res/raw-'),
-						rawAssets: self._CCSettings.rawAssets
-					});
-
-					var launchScene = self._CCSettings.launchScene;
-
-					// load scene
-					cc.director.loadScene(launchScene, null,
-						function () {
-							if (cc.sys.isBrowser) {
-								// show canvas
-								canvas.style.visibility = '';
-								var div = document.getElementById('GameDiv');
-								if (div) {
-									div.style.backgroundImage = '';
-								}
-							}
-
-							// play game
-							// cc.game.resume();
-
-							console.log('Success to load scene: ' + launchScene);
-						}
-					);
-
-					// purge
-					//noinspection JSUndeclaredVariable
-					self._CCSettings = undefined;
-				};
-
-				var option = {
-					//width: width,
-					//height: height,
-					id: 'GameCanvas',
-					scenes: self._CCSettings.scenes,
-					debugMode: self._CCSettings.debug ? cc.DebugMode.INFO : cc.DebugMode.ERROR,
-					showFPS: self._CCSettings.debug,
-					frameRate: 60,
-					jsList: [
-						self.resolveUrl('js/project.js')
-					],
-					groupList: self._CCSettings.groupList,
-					collisionMatrix: self._CCSettings.collisionMatrix
-				};
-
-				cc.game.run(option, onStart);
 			}
+
+			// init engine
+			var canvas, div;
+			//var width = 640, height = 480;
 
 			if (cc.sys.isBrowser) {
-				boot();
-			}
-			else if (cc.sys.isNative) {
-				require('js/settings.js');
-				require('js/jsb_polyfill.js');
+				canvas = document.getElementById('GameCanvas');
+				div = document.getElementById('GameDiv');
 
-				boot();
+				//width = div.clientWidth;
+				//height = div.clientHeight;
 			}
 
-		}, 1000);
+			function setLoadingDisplay () {
+				// Loading splash scene
+				var splash = document.getElementById('splash');
+				var progressBar = splash.querySelector('.progress-bar span');
+				var currentResCount = cc.loader.getResCount();
+				cc.loader.onProgress = function (completedCount, totalCount, item) {
+					var percent = 100 * (completedCount - currentResCount) / (totalCount - currentResCount);
+					if (progressBar) {
+						progressBar.style.width = percent.toFixed(2) + '%';
+					}
+				};
+				splash.style.display = 'block';
+
+				cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
+					splash.style.display = 'none';
+				});
+			}
+
+			var onStart = function () {
+				cc.view.resizeWithBrowserSize(true);
+				// UC browser on many android devices have performance issue with retina display
+				if (cc.sys.os !== cc.sys.OS_ANDROID || cc.sys.browserType !== cc.sys.BROWSER_TYPE_UC) {
+					cc.view.enableRetina(true);
+				}
+				//cc.view.setDesignResolutionSize(self._CCSettings.designWidth, self._CCSettings.designHeight, cc.ResolutionPolicy.SHOW_ALL);
+			
+				if (cc.sys.isBrowser) {
+					setLoadingDisplay();
+				}
+
+				if (self._CCSettings.orientation === 'landscape') {
+					cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
+				}
+				else if (self._CCSettings.orientation === 'portrait') {
+					cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
+				}
+
+				// init assets
+				cc.AssetLibrary.init({
+					libraryPath: self.resolveUrl('res/import'),
+					rawAssetsBase: self.resolveUrl('res/raw-'),
+					rawAssets: self._CCSettings.rawAssets
+				});
+
+				var launchScene = self._CCSettings.launchScene;
+
+				// load scene
+				cc.director.loadScene(launchScene, null,
+					function () {
+						if (cc.sys.isBrowser) {
+							// show canvas
+							canvas.style.visibility = '';
+							var div = document.getElementById('GameDiv');
+							if (div) {
+								div.style.backgroundImage = '';
+							}
+						}
+
+						// play game
+						// cc.game.resume();
+
+						console.log('Success to load scene: ' + launchScene);
+					}
+				);
+
+				// purge
+				//noinspection JSUndeclaredVariable
+				self._CCSettings = undefined;
+			};
+
+			var option = {
+				//width: width,
+				//height: height,
+				id: 'GameCanvas',
+				scenes: self._CCSettings.scenes,
+				debugMode: self._CCSettings.debug ? cc.DebugMode.INFO : cc.DebugMode.ERROR,
+				showFPS: self._CCSettings.debug,
+				frameRate: 60,
+				jsList: [
+					self.resolveUrl('js/project.js')
+				],
+				groupList: self._CCSettings.groupList,
+				collisionMatrix: self._CCSettings.collisionMatrix
+			};
+
+			cc.game.run(option, onStart);
+		}
+
+		if (cc.sys.isBrowser) {
+			boot();
+		}
+		else if (cc.sys.isNative) {
+			require('js/settings.js');
+			require('js/jsb_polyfill.js');
+
+			boot();
+		}
 	}
 });
