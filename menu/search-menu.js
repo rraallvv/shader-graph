a8c63aaec90bc1d8b47344806d7ec2c37b2aa708
Developer <>
2016-07-15 16:32:41 -0400

    remove unused stuff

--- menu/search-menu.js
+++ menu/search-menu.js
@@ -2,15 +2,6 @@
 
 "use strict";
 
-/*
-function addNode(type) {
-	parent.shaderGraph.addNode({
-		type: type,
-		pos: [clickCoordsX, clickCoordsY]
-	});
-}
-*/
-
 var contextMenuClassName = "context-menu";
 var contextMenuItemClassName = "menu-item";
 var contextMenuItemHoverClassName = "menu-item-hover";
@@ -52,161 +43,10 @@ var fuseOptions = {
   keys: ["type"]
 };
 
-/*
-function graphReadyListerner() {
-	window.addEventListener('WebComponentsReady', function(e) {
-		parent.shaderGraph = document.getElementById("graph");
-		if (parent.shaderGraph) {
-			// build the list of nodes
-			var menu = document.getElementById(contextMenuItemsClassName);
-			nodeTypes = parent.shaderGraph.nodeList();
-			for (var i = 0; i < nodeTypes.length; i++) {
-				var a = document.createElement("a");
-				a.type = a.innerHTML = nodeTypes[i].type;
-				a.className = contextMenuItemClassName;
-				a.onclick = function (e) {
-					addNode(this.type);
-					toggleMenuOff();
-					e.stopPropagation();
-				};
-				a.onmouseover = function () {
-					clearHoveredMenuItems();
-					this.isHovered = true;
-					this.classList.add(contextMenuItemHoverClassName);
-				}
-				a.onmouseout = function () {
-					this.isHovered = false;
-					this.classList.remove(contextMenuItemHoverClassName);
-				}
-				a.onmousemove = function (e) {
-					if (!this.isHovered) {
-						this.onmouseover(e);
-					}
-				}
-				menu.appendChild(a);
-			}
-		}
-
-		parent.preview.onload = function(){
-			parent.shaderGraph.updateShader();
-		};
-		
-		parent.preview.init();
-
-		parent.shaderGraph.onShaderUpdate = function(shader){
-			var shaderDef = shader.buildShader()
-			var source = document.getElementById("source");
-			source.innerHTML = ShaderGraph.Beautify(
-				optimize_glsl(shaderDef.fshader(), "2", "fs"),
-				//fs,
-				{
-					"indent_size": 1,
-					"indent_char": '\t',
-					"max_preserve_newlines": -1,
-					"brace_style": "expand",
-					"wrap_line_length": 0
-				}
-			);
-
-			source.classList.remove("prettyprinted");
-			prettyPrint();
-
-			parent.preview.updateShader(shaderDef);
-		};
-
-		parent.shaderGraph.onConnectionReleased = function(e) {
-			toggleMenuOn();
-			positionMenu(e);
-			return false;
-		};
-		
-		//*
-		var demos = [
-			{ name: "Pattern",
-				nodes: [
-					{type:"fragColor", pos:[650, 130]},
-					{type:"value", pos:[0, 0], value:70},
-					{type:"uv", pos:[0, 100]},
-					{type:"value", pos:[0, 220], value:35},
-					{type:"value", pos:[0, 320], value:0.5},
-					{type:"multiply", pos:[200, 50]},
-					{type:"multiply", pos:[200, 150]},
-					{type:"cos", pos:[350, 50]},
-					{type:"sin", pos:[350, 150]},
-					{type:"join", pos:[500, 100]},
-					{type:"value", pos:[320, 250], value: 1}
-				],
-				links: [
-					[1, 5],
-					[2.1, 5.1],
-					[2.2, 6],
-					[3, 6.1],
-					[5, 7],
-					[6, 8],
-					[7, 9],
-					[8, 9.1],
-					[4, 9.2],
-					[10, 9.3],
-					[9, 0]
-				]
-			},
-			{ name: "Black and White",
-				nodes: [
-					{type:"fragColor", pos:[660, 200]},
-					{type:"texture", pos:[0, 0]},
-					{type:"split", pos:[0, 170]},
-					{type:"add", pos:[135, 90]},
-					{type:"add", pos:[267, 90]},
-					{type:"divide", pos:[400, 90]},
-					{type:"value", pos:[250, 175], value:3},
-					{type:"join", pos:[530, 170]}
-				],
-				links: [
-					[1, 2],
-					[2, 3],
-					[2.1, 3.1],
-					[3, 4],
-					[2.2, 4.1],
-					[4, 5],
-					[6, 5.1],
-					[5, 7],
-					[5, 7.1],
-					[5, 7.2],
-					[2.3, 7.3],
-					[7, 0]
-				]
-			}];
-
-		var placeholder = document.getElementById("demos");
-
-		var element = document.createElement("a");
-		element.onclick = function() {
-			parent.shaderGraph.clearGraph();
-		};
-		element.innerHTML = "Clear";
-		element.className = "demo";
-		element.style.color = "red";
-		placeholder.appendChild(element);
-
-		for (var i = 0; i < demos.length; i++) {
-			var element = document.createElement("a");
-			element.demo = demos[i];
-			element.onclick = function() {
-				parent.shaderGraph.loadGraph(this.demo);
-			};
-			element.innerHTML = element.demo.name;
-			element.className = "demo";
-			placeholder.appendChild(element);
-		}
-	});
-}
-*/
-
 Editor.polymerElement({
 	ready: function() {
 		menu = this.$["context-menu"];
 		this._initSearchListeners();
-		// graphReadyListerner();
 		this._initContextListener();
 		this._initClickListener();
 		this._initKeyupListener();
