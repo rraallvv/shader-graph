4108923123dd5da11cce4a4e791dbb625d4b4dbe
Developer <>
2016-07-28 23:48:35 -0400

    add dummy wire component

---
+++ editor/js/wire.js
@@ -0,0 +1,18 @@
+(function(){
+
+if ( typeof Editor === "undefined" ) {
+	window.Editor = { polymerElement: Polymer, log: console.log };
+}
+
+Editor.polymerElement({
+	ready: function() {
+	},
+	attached: function() {
+	},
+	detached: function() {
+	},
+	properties: {
+	}
+});
+
+})();
